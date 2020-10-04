df9da8ac52ac370cbd9b2a1ffdc02907f7785188
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-10-04 10:59:03 +0530

    init HomeScreen

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>This is Homescreen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
