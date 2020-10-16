895596665c3ca7f628acc462701da23789e4dd60
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-10-16 16:54:09 +0530

    sample api call

--- src/screens/InquiriesScreen.js
+++ src/screens/InquiriesScreen.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import axios from "axios";
+import api from "../api";
 
 export function InquiriesScreen({ navigation }) {
+  useEffect(() => {
+    //sample api call to retrive sites
+    axios
+      .get(api.concat(`/api/sites`))
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
   return (
     <View style={styles.container}>
       <Text>This is InquiriesScreen</Text>
